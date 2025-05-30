4b32df72a7b22e1b93ee9aeb8e08a222054ad165
williamckha <williamha@outlook.com>
2025-05-29 17:19:37 -0700

    Implement single data swap instruction

--- pyboy_advance/cpu/arm/swp.py
+++ pyboy_advance/cpu/arm/swp.py
@@ -2,9 +2,30 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from pyboy_advance.memory.memory import MemoryAccess
+from pyboy_advance.utils import get_bit, get_bits
+
 if TYPE_CHECKING:
     from pyboy_advance.cpu.cpu import CPU
 
 
 def arm_single_data_swap(cpu: CPU, instr: int):
-    return
+    """Executes a Single Data Swap instruction (SWP)"""
+
+    base_reg = get_bits(instr, 16, 19)
+    dst_reg = get_bits(instr, 12, 15)
+    src_reg = get_bits(instr, 0, 3)
+
+    byte_word_bit = get_bit(instr, 22)
+
+    if byte_word_bit:  # Byte swap
+        temp = cpu.memory.read_8(cpu.regs[base_reg], MemoryAccess.NON_SEQUENTIAL)
+        cpu.memory.write_8(cpu.regs[base_reg], cpu.regs[src_reg], MemoryAccess.NON_SEQUENTIAL)
+        cpu.regs[dst_reg] = temp
+    else:  # Word swap
+        temp = cpu.memory.read_32_ror(cpu.regs[base_reg], MemoryAccess.NON_SEQUENTIAL)
+        cpu.memory.write_32(cpu.regs[base_reg], cpu.regs[src_reg], MemoryAccess.NON_SEQUENTIAL)
+        cpu.regs[dst_reg] = temp
+
+    cpu.arm_advance_pc()
+    cpu.next_fetch_access = MemoryAccess.NON_SEQUENTIAL
